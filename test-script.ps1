c1ae04393cee2da459033bbd8f60ccec74108c6b
Kiran Gonela <kirangonela@Kirans-MacBook-Pro.local>
2023-04-05 16:03:48 +0100

    first commit

--- test-script.ps1
+++ test-script.ps1
@@ -1,27 +1,30 @@
 param($totalHours = 1)
 
+$myshell = New-Object -ComObject wscript.shell
+
 # Start Notepad
 $notepad = Start-Process notepad -PassThru
 
 # Wait for the Notepad process to start
 Start-Sleep -Seconds 2
 
-# Bring the Notepad window to the foreground
-[System.Runtime.InteropServices.Marshal]::GetActiveObject("WScript.Shell").AppActivate($notepad.Id)
-
 # Calculate the total number of iterations
 $iterations = $totalHours * 30
 
 for ($i = 0; $i -lt $iterations; $i++) {
+    # Bring the Notepad window to the foreground
+    $myshell.AppActivate($notepad.Id)
+
     # Send a period key (".") every 2 minutes
     Start-Sleep -Seconds 120
-    [System.Runtime.InteropServices.Marshal]::GetActiveObject("WScript.Shell").SendKeys(".")
+    $myshell.SendKeys(".")
 
     # After every 60 minutes (30 iterations), send an Enter key to start a new line
     if (($i + 1) % 30 -eq 0) {
-        [System.Runtime.InteropServices.Marshal]::GetActiveObject("WScript.Shell").SendKeys("{ENTER}")
+        $myshell.SendKeys("{ENTER}")
     }
 }
 
 # Close Notepad without saving changes
-[System.Runtime.InteropServices.Marshal]::GetActiveObject("WScript.Shell").SendKeys("%{F4}n")
+$myshell.AppActivate($notepad.Id)
+$myshell.SendKeys("%{F4}n")
